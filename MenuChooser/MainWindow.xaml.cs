e263bbbb7b4ba9cf89e5ef02516d9228654d24eb
Droideight <vincentking700@gmail.com>
2022-03-17 00:41:52 +0800

    加入專案檔案。

---
+++ MenuChooser/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MenuChooser
+{
+    class MenuItem
+    {
+        public Random Randomizer = new Random();
+        public string[] Proteins = { "Roast beef", "Salami", "Turkey", "Ham", "Pastrami", "Tofu" };
+        public string[] Condiments = { "yellow mustard", "brown mustard", "honey mustard", "mayo", "relish", "french dressing" };
+        public string[] Bread = { "rye", "white", "wheat", "pumppernickel", "a roll" };
+        public string Description = "";
+        public string Price;
+        public void Generate()
+        {
+            string randomProtein = Proteins[Randomizer.Next(Proteins.Length)];
+            string randomCondiment = Condiments[Randomizer.Next(Condiments.Length)];
+            string randomBread = Bread[Randomizer.Next(Bread.Length)];
+            Description = randomProtein + " with "+randomCondiment + " on "+randomBread;
+            decimal bucks = Randomizer.Next(2, 5);
+            decimal cents = Randomizer.Next(1, 98);
+            decimal price = bucks + (cents * .01M);
+            Price = price.ToString("c");
+        }
+    }
+}
