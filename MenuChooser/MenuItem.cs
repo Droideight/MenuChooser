c1f77b31b0ed551cae5863bf2c067217d12e809a
Droideight <vincentking700@gmail.com>
2022-03-17 02:07:19 +0800

    Finalized Project.

--- MenuChooser/MenuItem.cs
+++ MenuChooser/MenuItem.cs
@@ -8,17 +8,17 @@ namespace MenuChooser
 {
     class MenuItem
     {
-        public Random Randomizer = new Random();
+        public static Random Randomizer = new Random();
         public string[] Proteins = { "Roast beef", "Salami", "Turkey", "Ham", "Pastrami", "Tofu" };
         public string[] Condiments = { "yellow mustard", "brown mustard", "honey mustard", "mayo", "relish", "french dressing" };
-        public string[] Bread = { "rye", "white", "wheat", "pumppernickel", "a roll" };
+        public string[] Breads = { "rye", "white", "wheat", "pumppernickel", "a roll" };
         public string Description = "";
         public string Price;
         public void Generate()
         {
             string randomProtein = Proteins[Randomizer.Next(Proteins.Length)];
             string randomCondiment = Condiments[Randomizer.Next(Condiments.Length)];
-            string randomBread = Bread[Randomizer.Next(Bread.Length)];
+            string randomBread = Breads[Randomizer.Next(Breads.Length)];
             Description = randomProtein + " with " + randomCondiment + " on " + randomBread;
             decimal bucks = Randomizer.Next(2, 5);
             decimal cents = Randomizer.Next(1, 98);
